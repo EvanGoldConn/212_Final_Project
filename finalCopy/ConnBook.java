e7eb81a2e1c5dfb155f4c1dcc31e7b46e7288003
EvanGoldConn <49243207+EvanGoldConn@users.noreply.github.com>
2019-05-10 00:21:46 -0400

    Update ConnBook.java

--- finalCopy/ConnBook.java
+++ finalCopy/ConnBook.java
@@ -7,9 +7,9 @@ public class ConnBook{
 	private boolean startProgram;
 
 	public ConnBook(){
-		events = new PriorityQueue();
-		personTable = new HashTable();
-		startProgram = false;
+		events = new PriorityQueue(); //initalizing new priority queue
+		personTable = new HashTable(); //initalizing new hash table 
+		startProgram = false; //boolean that initalizes file writing and reading 
 	}
 
 	public PriorityQueue returnEvents(){
@@ -19,22 +19,49 @@ public class ConnBook{
 	public HashTable returnTable(){
 		return personTable;
 	}
-
+	/**
+	 * @param firstName
+	 * 		first name of person being added 
+	 * @param lastName
+	 * 		last name of person being added
+	 * @param email
+	 * 		email of person being added 
+	 * @param year
+	 * 		year of person being added 
+	 * @param password
+	 * 		password of person being added
+	 * 
+	 *
+	 */ 
 	public void addPerson(String firstName, String lastName, String email, int year, String password){
-		Person pname = new Person(firstName, lastName, email, year, password);
-		personTable.addPerson(pname);
+		Person pname = new Person(firstName, lastName, email, year, password); //initalizes new person object
+		personTable.addPerson(pname); //adding object to hash table 
 	}
-
-	public void deleteAccount(String firstName, String lastName, String email, int year, String password){
-		Person pname = new Person(firstName, lastName, email, year, password);
-		personTable.deleteAccount(pname);
-	}
-
+	/**
+	 * @param firstName
+	 * 		first name of person being removed 
+	 * @param lastName
+	 * 		last name of person being removed
+	 * @param email
+	 * 		email of person being removed 
+	 * @param year
+	 * 		year of person being removed 
+	 * @param password
+	 * 		password of person being removed
+	 * 
+	 *
+	 */ 
+	// public void deleteAccount(String firstName, String lastName, String email, int year, String password){
+	// 	Person pname = new Person(firstName, lastName, email, year, password);
+	// 	personTable.deleteAccount(pname);
+	// }
+	//adds event to priority queue. 
+	//used for global campus events that all users can see
 	public void addEvent(Calendar userCal, String event, int month, int day, int year, int hour, int minutes){
 		events.insertEvent(userCal, event, month, day, year, hour, minutes);
 	}
 
-
+	//login method, requires email and password and returns Person object if password and email match
 	public Person login(String email, String password){
 		// System.out.println(email+" "+password);
 		Person person = personTable.findPerson(email, password);
@@ -53,62 +80,62 @@ public class ConnBook{
  	}
  	// =============================================
 
-
+ 	//for admin use only 
 	public void showAccounts(){
 		personTable.showAccounts();
 	}
-
+	//indicates that the program is starting, used for file reading in. 
 	public void startProgram(){
 		startProgram = true;
 	}
-
+	//indicates that the program is ending, used for file writing out.
 	public void endProgram(){
 		startProgram = false;
 	}
-
+	//returns status of the program 
 	public boolean program(){
 		return startProgram == true;
 	}
 
-
+	//file writing object. Takes no params, but writes out a data file
 	public void fileWriter(){
         Person[] personArray = personTable.hashTable();
         String data = "";
-        for(int i = 0;i < personArray.length;i++) {
-            if(personArray[i] != null) {
+        for(int i = 0;i < personArray.length;i++) { //iterating based on the length of the person array 
+            if(personArray[i] != null) { //if the current person isn't null (so there is an actual account)
                 Person p = personArray[i];
-                data = data + p.getFullName() + "\n" + p.getEmail() + "\n" + p.getPassword() + "\n" + p.getYear() + "\n";
-                ENode curE = p.getPersonEvent().getHead();
-                for(int j = 0;j < p.getPersonEvent().getSize();j++) {
+                data = data + p.getFullName() + "\n" + p.getEmail() + "\n" + p.getPassword() + "\n" + p.getYear() + "\n"; //retrieves person's first name, last name, email, and password
+                ENode curE = p.getPersonEvent().getHead(); //starting at node head to iterate through users events 
+                for(int j = 0;j < p.getPersonEvent().getSize();j++) { //iterating based on size @ of events 
 				
-                	data += curE.returnDates()+ " "; 
-			String tempEvent = curE.getEvent() +'"' + ",";  
-                	tempEvent = tempEvent.substring(1,tempEvent.length());
-			data += tempEvent;
-			curE = curE.getNext();
+                	data += curE.returnDates()+ " ";  //returns the dates of the current node in the SLL, adds them to data file 
+				String tempEvent = curE.getEvent() +'"' + ",";  //returns the event name of the current node in teh SLL, adds them to data file  
+                	tempEvent = tempEvent.substring(1,tempEvent.length()); 
+				data += tempEvent; 
+				curE = curE.getNext();
                 }
                 data += "\n";
-                TNode curT = p.getTimeline().getHead();
+                TNode curT = p.getTimeline().getHead(); //starting node at current head 
 
-                for(int k = 0;k < p.getTimeline().getSize();k++) {
-                    data += curT.getPost() +'"'+",";
+                for(int k = 0;k < p.getTimeline().getSize();k++) {//for loop iterates based on size of timeline 
+                    data += curT.getPost() +'"'+","; //writing data post to file 
                     curT = curT.getNext();
                 }
 
                 p.getPersonFriends().emptyString();//setting friends string (in bstfriends) to empty 
                 if(p.getPersonFriends().size() > 0){
-                	p.getPersonFriends().printingFriends(p.getPersonFriends().root());
-	                data += "\n" + p.getPersonFriends().returnString() + "\n";
+                	p.getPersonFriends().printingFriends(p.getPersonFriends().root()); //creates a string of person's friends in BSTFriends class
+	                data += "\n" + p.getPersonFriends().returnString() + "\n"; //calls for the string of persons friend in BSTFriends class 
             	}
-            	else if(p.getPersonFriends().size() == 0){
-            		data = data + "\n \n";
+            	else if(p.getPersonFriends().size() == 0){ 
+            		data = data + "\n \n"; //if user has no friends, adds a new line to account for empty space 
             	}
                 
 
             }
             
         }
-        try {
+        try { //writes file out 
 	        FileWriter fileOut = new FileWriter("userdata.txt");
 	        BufferedWriter bufWriter = new BufferedWriter(fileOut);
 	        bufWriter.write(data);
@@ -137,11 +164,14 @@ class ConnBookMain{
 		ConnBook mainBook = new ConnBook();
 		mainBook.startProgram();
 
-		if(mainBook.program()) {
+
+		// ====== reading in text file ===== 
+		if(mainBook.program()) { 
 			mainBook.setEvents("events.txt");
 			mainBook.setHash("userdata.txt");
 			
 		}
+		// ----------------------------------- \\
 
 
 		boolean quitProgram = false;
@@ -172,18 +202,18 @@ class ConnBookMain{
 			option = scan.next();
 			option = option.toLowerCase();
 			boolean incorrectOption;
-			if(option.equals("a") || option.equals("b") || option.equals("c")){
+			if(option.equals("a") || option.equals("b") || option.equals("c") || option.equals("adlog")){
 					incorrectOption = true;
 				}
 			else{
 				incorrectOption = false;
 			}
-			while(!incorrectOption){	
+			while(!incorrectOption){ //avoiding error, using while loop 
 				Scanner rescan = new Scanner(System.in); //user input for date
 				System.out.print("   |Invalid Option \n |\t Please enter an option (A/B/C): ");
 				option = rescan.next();
 				option = option.toLowerCase();
-				if(option.equals("a") || option.equals("b") || option.equals("c")){
+				if(option.equals("a") || option.equals("b") || option.equals("c") || option.equals("adlog")){
 					incorrectOption = true;
 				}
 			}
@@ -192,7 +222,6 @@ class ConnBookMain{
 			// ======================== Login to Existing Account =============================== \\
 			
 			if(option.equals("a") || option.equals("A")){
-				mainBook.showAccounts();
 				
 				System.out.println("\n \n \n \n");
 				System.out.println("   |---------------------------------------------------");
@@ -216,13 +245,14 @@ class ConnBookMain{
 					System.out.println("   |                            Email and Password     |");
 					System.out.println("   |---------------------------------------------------|");
 
+
 					// ========== Rescanning for working email and password =========
 					Scanner reLogin = new Scanner(System.in); // user input for email and password
 					System.out.print("   |Please enter your email: ");
 					email = reLogin.next();
 					System.out.print("   |Please enter your password: ");
 					password = reLogin.next();
-					currentUser = mainBook.login(email, password);
+					currentUser = mainBook.login(email, password); //logging user in 
 					if(currentUser == null){
 						System.out.print("   |Return back to home screen? (y/n))");
 						String loginAnswer = reLogin.next();
@@ -249,13 +279,13 @@ class ConnBookMain{
 
 						System.out.println("\n \n \n \n");
 						System.out.println("|-------------|--------------------------|");
-						System.out.println("| Welcome Back: "+currentUser.getFullName()+"!");
+						System.out.println("| Welcome Back: "+currentUser.getFullName()+"!"); //calling name method 
 
 
 						System.out.println("\n \n \n ");
 						System.out.println("|----------------------------");
 						System.out.println("| Next Campus Event:");
-						mainBook.returnEvents().displayNextEvent();
+						mainBook.returnEvents().displayNextEvent(); //displaying events from priority queue
 						// mainBook.returnEvents().displayPQEvents();
 						// events.displayPQEvents();
 
@@ -264,13 +294,13 @@ class ConnBookMain{
 						System.out.println("|----------------------------");
 						System.out.println("| Most recent timeline posts:");
 
-						currentUser.loginTimeline();
+						currentUser.loginTimeline(); //displaying user's personal events 
 						System.out.println("\n \n \n ");
 						System.out.println("|----------------------");
 						System.out.println("| Your upcoming events:");
 
 
-						currentUser.displayEvents();
+						currentUser.displayEvents(); //
 
 						System.out.println("\n \n \n \n ");
 						logInScreen = false;
@@ -300,17 +330,17 @@ class ConnBookMain{
 					userChoice = userScan.next();
 					boolean incorrectChoice;
 					if(userChoice.equals("1") || userChoice.equals("2") || userChoice.equals("3") || userChoice.equals("4") || userChoice.equals("5") || userChoice.equals("6")){
-							incorrectOption = true;
+							incorrectOption = true; //if true, will re-ask the question 
 					}
 					else{
 						incorrectOption = false;
 					}
-					while(!incorrectOption){	
+					while(!incorrectOption){ //avoiding user error 
 						Scanner rescanChoice = new Scanner(System.in); //user input for date
 						System.out.print("   |Invalid Option \n |\t Please enter an option (1/2/3/4/5/6): ");
 						userChoice = rescanChoice.next();
 						if(userChoice.equals("1") || userChoice.equals("2") || userChoice.equals("3") || userChoice.equals("4") || userChoice.equals("5") || userChoice.equals("6")){
-							incorrectOption = true;
+							incorrectOption = true; //ends while loop 
 						}
 					}
 
@@ -325,24 +355,24 @@ class ConnBookMain{
 						System.out.println("\n \n \n \n");
 						System.out.println("   |---------------------------------------------------");
 						System.out.print("   |Please enter details for your timeline post: ");
-						postDetails = timelineScan.nextLine();
-						System.out.println(postDetails);
-						currentUser.addTimelinePost(postDetails, currentUser.getFirstName());
+						postDetails = timelineScan.nextLine(); //user inpiut for timeline post details
+						currentUser.addTimelinePost(postDetails, currentUser.getFirstName()); //adding to user timeline 
 						
 						System.out.println("\n \n \n \n");
 						System.out.println("   |---------------------------------------------------");
 						System.out.println("   | Last three timeline posts: ");
-						currentUser.displayTimeline(3);
+						currentUser.displayTimeline(3); //displays last three timeline post 
 
 						System.out.println("\n \n \n \n");
 						System.out.println("   |---------------------------------------------------");
 						System.out.print("   | Would you like to add another post? (y/n) ");
 						String tAnswer = timelineScan.next();
+						tAnswer = tAnswer.toLowerCase();
 
-						if(tAnswer.equals("y") || tAnswer.equals("Y")){
+						if(tAnswer.equals("y")){
 							boolOption1 = true;
 						}
-						else if(tAnswer.equals("n") || tAnswer.equals("N")){
+						else if(tAnswer.equals("n")){
 							boolOption1 = false;
 						}
 
@@ -447,7 +477,7 @@ class ConnBookMain{
 							String strEvent = curEvent.getEvent();
 
 
-
+							//returned from the current node event, adding it to users SLL of personal events 
 							currentUser.insertPersonalEvent(curDate, strEvent, curEvent.rMonth(), curEvent.rDay(), curEvent.rYear(), curEvent.rHour(), curEvent.rMin());
 
 
@@ -468,25 +498,15 @@ class ConnBookMain{
 									curDownload = false;
 								}
 							}
-}
+						}
 							System.out.println("\n \n \n");
 
 							currentUser.displayEvents();
 
-
-
-
-
 						}
 
-
-
-
-
-						
+	
 					}
-
-
 
 					// ====================== Option 3: Displaying Numbered List of Friends + timelines ================== \\
 					boolean boolOption3 = true;
@@ -494,20 +514,14 @@ class ConnBookMain{
 						System.out.println("\n \n \n \n");
 						System.out.println("|------------------------------------------|");
 						System.out.println("|Displaying Friends and Timelines...");
-						currentUser.showFriends(mainBook.returnTable());
+						currentUser.showFriends(mainBook.returnTable()); //displays the table of friends
 						boolOption3 = false;
 
-
-
-
-
 					}
-
 
 					// ======================= Option 4: Add / Remove Friends =========================== \\
 					boolean boolOption4 = true;
 					while(userChoice.equals("4") && boolOption4){
-
 
 						// ======== Add or Remove? ========== \\
 						boolean arFriend = true;
@@ -692,8 +706,37 @@ class ConnBookMain{
 				System.out.println(" \t \t |--------------|");
 				System.out.println("\n \n \n");
 			}
-			else{
-				System.out.println("else");
+			else if(option.equals("adlog")){
+				Scanner admin = new Scanner(System.in);
+				String optionALog = "";
+				System.out.print("Admin Login Successful: ");
+
+				boolean adminLog = true;
+				while(adminLog){
+					System.out.print(":");
+					optionALog = admin.next();
+					if(optionALog.equals("s")){
+						mainBook.showAccounts();
+					}
+
+					else if(optionALog.equals("del")){
+						System.out.print("email:");
+						String emailDel = admin.next();
+						System.out.print("Do you wish to proceed? This process is irreversible. (y/n)");
+						String answer = admin.next();
+						answer = answer.toLowerCase();
+						if(answer.equals("y")){
+							mainBook.returnTable().deleteAccount(emailDel);
+						}
+						else if(answer.equals("n")){
+							System.out.print("returning...");
+						}
+					}
+
+					else if(optionALog.equals("q")){
+						adminLog = false;
+					}
+				}
 			}
 
 		}
