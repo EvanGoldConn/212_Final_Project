806219c98e2b9829d66b7fb8fb3fb470408ba994
bgould7 <49792993+bgould7@users.noreply.github.com>
2019-05-09 22:36:42 -0400

    Update Events.java

--- finalCopy/Events.java
+++ finalCopy/Events.java
@@ -24,50 +24,61 @@ public class Events{
 		return size == 0;
 	}
 
-	/*adds a new event
-	@param date  
-		Calendar object with actual current date
-	@param event
-		String with the name of the event
-	@param month1
-		Integer with the month of the event
-	@param day1
-		Integer with the day of the event
-	@param year1
-		Integer with the year 
-	@param hour1
-		Integer with the hour
-	@param minutes1
-		Integer with the minutes
+	/**
+	*method addEvent, adds a new event
+	*@param date  
+	*	Calendar object with actual current date
+	*@param event
+	*	String with the name of the event
+	*@param month1
+	*	Integer with the month of the event
+	*@param day1
+	*	Integer with the day of the event
+	*@param year1
+	*	Integer with the year 
+	*@param hour1
+	*	Integer with the hour
+	*@param minutes1
+	*	Integer with the minutes
 	*/
 	public void addEvent(Calendar date, String event, int month1, int day1, int year1, int hour1, int minutes1){
 		ENode u = new ENode(date, event, null, month1, day1, year1, hour1, minutes1);
-		if(isEmpty()){
-			head = u;
+		if(isEmpty()){ //if it is empty
+			head = u; //make this new event the head ENode
 		}
-		else if (!isEmpty()){ 
-			ENode temp = head;
-			head = u;
+		else if (!isEmpty()){ //if it isn't empty 
+			ENode temp = head; //make temp the head
+			head = u; //set the new event to the head
 			head.setNext(temp);
 		}
 		size++;
 	}
+	
+	/**
+	 * method getHead, gets the head of the LL
+	 * @return head
+	 */
 	public ENode getHead() {
 		return head;
 	}
 
+	/**
+	 * method getSize, gets the size of the LL
+	 * @return size
+	 */
 	public int getSize() {
 		return size;
 	} 
 
-	
-
+	/**
+	 * method displayEvents, shows all of the events
+	 */
 	public void displayEvents(){
 		ENode cur = head;
 
-		if (!isEmpty()){
-			for(int i = 0; i < size; i++){
-				if(cur != null){
+		if (!isEmpty()){ //if it isn't empty
+			for(int i = 0; i < size; i++){ //loops through all events
+				if(cur != null){ //prints out every event with the date and time
 					System.out.println(cur.getDate().getTime()+" "+cur.getEvent());
 					cur = cur.getNext();
 				}
@@ -76,13 +87,15 @@ public class Events{
 		}
 	}
 
-
+	/**
+	 * method getTimeInMillis, gets the time in milliseconds on ENode v
+	 */
 	public long getTimeInMillis(ENode v){
 		return v.getMiliTime();
 	}
 }
 
-class eventTest{
+class eventTest{ //test class for the events
 	public static void main(String[] args){
 		Events test = new Events();
 		Calendar userCal = Calendar.getInstance();  //new calendar instance
